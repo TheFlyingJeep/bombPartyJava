a4f1fea265eb627e4c4f23ca773e3e15b2fadc0c
grass <81576338+Grassias10@users.noreply.github.com>
2022-09-12 12:13:20 -0400

    changed to gridlayout and set basic gui layout

--- GUI.java
+++ GUI.java
@@ -3,18 +3,39 @@ import java.awt.*;
 
 public class GUI{
     JFrame frame;
-    JPanel panel;
+    GridLayout grid;
+    JLabel playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, timer;
+    JTextField playerInput;
 
     public GUI(){
         frame = new JFrame("Bomb Party (Java)");
-        panel = new JPanel();
+        grid = new GridLayout(4,3);
+        playerOne = new JLabel("Player 1");
+        playerTwo = new JLabel("Player 2");
+        playerThree = new JLabel("Player 3");
+        playerFour = new JLabel("Player 4");
+        playerFive = new JLabel("Player 5");
+        playerSix = new JLabel("Player 6");
+        playerSeven = new JLabel("Player 7");
+        playerEight = new JLabel("Player 8");
+        timer = new JLabel("timer");
+        playerInput = new JTextField(10);
 
-        panel.setBorder(BorderFactory.createEmptyBorder(250, 250, 250, 250));
-        panel.setLayout(new GridLayout(0, 1));
+        frame.setSize(500,500);
+        frame.setLayout(grid);
 
-        frame.add(panel, BorderLayout.CENTER);
+        frame.add(playerOne);
+        frame.add(playerTwo);
+        frame.add(playerThree);
+        frame.add(playerFour);
+        frame.add(timer);
+        frame.add(playerFive);
+        frame.add(playerSix);
+        frame.add(playerSeven);
+        frame.add(playerEight);
+        frame.add(playerInput);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setVisible(true);
     }
 
