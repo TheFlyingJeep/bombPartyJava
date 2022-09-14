4e3e6bc198f5ea3fedf4a77af9ebb3449ab189c4
grass <81576338+Grassias10@users.noreply.github.com>
2022-09-14 13:46:34 -0400

    added action listeners for textfield and button + updated GUI

--- GUI.java
+++ GUI.java
@@ -1,15 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GUI{
+public class GUI {
     JFrame frame;
     GridLayout grid;
-    JLabel playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, timer;
+    JLabel playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, timer, letters;
     JTextField playerInput;
+    JButton enter;
 
+    String guess = "";
+
+    //building game window
     public GUI(){
         frame = new JFrame("Bomb Party (Java)");
-        grid = new GridLayout(4,3);
+        grid = new GridLayout(4,3); //row, columns
         playerOne = new JLabel("Player 1");
         playerTwo = new JLabel("Player 2");
         playerThree = new JLabel("Player 3");
@@ -19,10 +24,14 @@ public class GUI{
         playerSeven = new JLabel("Player 7");
         playerEight = new JLabel("Player 8");
         timer = new JLabel("timer");
+        lettersLeft = new JLabel("<html>A B C D E F G H I J K L<br/>M N O P Q R S T U V</html>");
         playerInput = new JTextField(10);
+        enter = new JButton("Enter");
 
-        frame.setSize(500,500);
+        frame.setSize(700,500); //width, height
         frame.setLayout(grid);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
         frame.add(playerOne);
         frame.add(playerTwo);
@@ -33,10 +42,47 @@ public class GUI{
         frame.add(playerSix);
         frame.add(playerSeven);
         frame.add(playerEight);
+        frame.add(lettersLeft);
         frame.add(playerInput);
+        frame.add(enter);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        enter.setActionCommand("enter");
+        enter.addActionListener(new enterButtonListener());
+
+        playerInput.setActionCommand("playerInput");
+        playerInput.addActionListener(new enterKeyListener());
+    }
+
+    //action listener for enter key
+    class enterKeyListener implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            guess = playerInput.getText();
+            System.out.println(guess);
+            playerInput.setText("");
+        }
+    }
+
+    //action listener for enter button
+    class enterButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            guess = playerInput.getText();
+            System.out.println(guess);
+            playerInput.setText("");
+        }
+    }
+
+    //updates the letters display on GUI
+    public void updateLetters(){
+        //params: letters used or letters unused by player from Player class
+    }
+
+    //refreshes GUI
+    public void refresh(){
+    }
+
+    //returns player's input aka player's guess
+    public String getGuess(){
+        return guess;
     }
 
     public static void main(String[] args){
