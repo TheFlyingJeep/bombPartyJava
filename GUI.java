cb95466b96362ddb0a5ea31fe5b40525fb75c9b0
grass <81576338+Grassias10@users.noreply.github.com>
2022-09-20 11:12:50 -0400

    new methods + layout updated

--- GUI.java
+++ GUI.java
@@ -1,20 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class GUI {
     JFrame frame;
     GridLayout grid;
-    JLabel playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, timer, letters;
+    JLabel playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, timer,
+            letters, wordDisplay, credits, hearts, logo;
     JTextField playerInput;
     JButton enter;
 
-    String guess = "";
+    String guess, lettersLeftDisplay;
+    ArrayList<String> lettersLeft = new ArrayList<String>();
 
     //building game window
     public GUI(){
         frame = new JFrame("Bomb Party (Java)");
-        grid = new GridLayout(4,3); //row, columns
+        grid = new GridLayout(6,3); //row, columns
         playerOne = new JLabel("Player 1");
         playerTwo = new JLabel("Player 2");
         playerThree = new JLabel("Player 3");
@@ -24,9 +27,13 @@ public class GUI {
         playerSeven = new JLabel("Player 7");
         playerEight = new JLabel("Player 8");
         timer = new JLabel("timer");
-        letters = new JLabel("<html>A B C D E F G H I J K L<br/>M N O P Q R S T U V</html>");
-        playerInput = new JTextField(10);
+        letters = new JLabel("<html>A B C D E F G H I J K<br/>L M N O P Q R S T U</html>");
+        playerInput = new JTextField();
         enter = new JButton("Enter");
+        wordDisplay = new JLabel(guess);
+        credits = new JLabel("BombParty (Java) was inspired by JKLM's BombParty");
+        hearts = new JLabel("hearts");
+        logo = new JLabel("blow up bash!");
 
         frame.setSize(700,500); //width, height
         frame.setLayout(grid);
@@ -43,8 +50,12 @@ public class GUI {
         frame.add(playerSeven);
         frame.add(playerEight);
         frame.add(letters);
+        frame.add(hearts);
+        frame.add(logo);
+        frame.add(credits);
         frame.add(playerInput);
         frame.add(enter);
+        frame.add(wordDisplay);
 
         enter.setActionCommand("enter");
         enter.addActionListener(new enterButtonListener());
@@ -59,6 +70,7 @@ public class GUI {
             guess = playerInput.getText();
             System.out.println(guess);
             playerInput.setText("");
+            refresh();
         }
     }
 
@@ -68,16 +80,23 @@ public class GUI {
             guess = playerInput.getText();
             System.out.println(guess);
             playerInput.setText("");
+            refresh();
         }
     }
 
     //updates the letters display on GUI
     public void updateLetters(){
-        //params: letters used or letters unused by player from Player class
+        //params: letters unused by player from Player class
+        for(int i = 0; i < lettersLeft.size(); i++){
+            lettersLeftDisplay += lettersLeft.get(i);
+            System.out.println(lettersLeftDisplay);
+        }
+        refresh();
     }
 
     //refreshes GUI
     public void refresh(){
+        wordDisplay.setText(guess); //displays word inputed on screens
     }
 
     //returns player's input aka player's guess
