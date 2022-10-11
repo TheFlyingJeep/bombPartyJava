488b8ef947e8fabea1cc8c84fc5b6d96beebe8dd
rabelauren <86250825+rabelauren@users.noreply.github.com>
2022-10-11 09:05:00 -0400

    10/10 push again

--- Player.java
+++ Player.java
@@ -1,23 +1,38 @@
 import java.util.*;
 import java.util.ArrayList;
-
+import java.net.Socket;
 
 class Player {
   String playerName = "";
   int lives = 3;
   ArrayList<String> lettersAvail = new ArrayList<String>();
   boolean isDead = false;
-  boolean isTurn = false; 
+  boolean isTurn = true;
   GUI gui = new GUI();
+  Client client;
+  Socket socket;
   String guess = "";
+  Thread thread;
+  String prompt = "";
 
-  public String getGuess() {
-    return gui.sendGuess();
+  public Player(Socket socket1, String name) {
+    playerName = name;
+    socket = socket1;
+    client = new Client(socket);
+    thread = new Thread(client);
+    thread.start();
+    theLoop();
   }
 
-  public void inputName(String name) {
-      playerName = name;
+  public void getGuess() {
+    if (gui.isValid()) {
+      guess = gui.sendCInput();
+      System.out.println(guess);
+      // gui.updateLives(lives - 1);
+      // System.out.println(lives);
     }
+
+  }
 
   public void letterAdd() {
     lettersAvail.add("A");
@@ -44,24 +59,27 @@ class Player {
     lettersAvail.add("U");
   }
 
-    public int lifeRemove(boolean timeGone) {
-      if (timeGone) {
-        lives--;
-      }
-      return lives;
+  public int lifeRemove(boolean timeGone) {
+    if (timeGone) {
+      lives--;
     }
+    return lives;
+  }
 
-    public int lifeAdd() {
-      if (lettersAvail.size() == 0) {
-        lives++; 
-      }
-      return lives;
+  public int lifeAdd() {
+    if (lettersAvail.size() == 0) {
+      lives++;
     }
+    return lives;
+  }
 
-    public ArrayList<String> updateUsedLetters(String used) {
+  public ArrayList<String> updateUsedLetters(String word) {
+    for (int j = 0; j < word.length(); j++) {
+      String letter = word.substring(j, j + 1);
+
       for (int i = 0; i < lettersAvail.size(); i++) {
         String a = lettersAvail.get(i);
-        if (used.equals(a)) {
+        if (letter.equals(a)) {
           lettersAvail.remove(i);
         }
       }
@@ -69,12 +87,65 @@ class Player {
       if (lettersAvail.size() == 0) {
         letterAdd();
       }
-      return lettersAvail;
+
     }
+    for (String i : lettersAvail) {
+      System.out.print(i + " ");
+    }
+    gui.updateLetters(lettersAvail);
+    return lettersAvail;
+  }
+
+  public String sendGuess() {
+    return guess;
+  }
+  /*
+   * public ArrayList<String> sepLetters(String word) {
+   * ArrayList<String> letters = new ArrayList<String>();
+   * for (int i = 0; i < word.length(); i++) {
+   * letters.add(word);
+   * }
+   * return letters;
+   * }
+   */
 
   public ArrayList<String> sendLetters() {
     return lettersAvail;
   }
 
-    
+  public void sendLives() {
+    // gui.updateLives(lives);
+  }
+
+  public boolean sendTurn() {
+    return isTurn;
+  }
+
+  public void sendPrompts() {
+    gui.setPrompt(prompt);
+  }
+
+  public void theLoop() {
+    while (!isDead) {
+      guess = gui.sendCInput();
+      if (!guess.equalsIgnoreCase("")) {
+        System.out.println(guess);
+        client.sendGuess(playerName, guess);
+        gui.resetGuesses();
+
+        prompt = client.prompt;
+        sendPrompts();
+        for (int i = 0; i < lettersAvail.size(); i++) {
+          String elem = lettersAvail.get(i);
+          System.out.println(elem);
+        }
+      }
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
 }
