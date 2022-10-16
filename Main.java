e1b621e25755061d74856abb5a6beac94a3ddf10
TheFlyingJeep <90123966+TheFlyingJeep@users.noreply.github.com>
2022-10-16 15:31:49 -0400

    irl egirl moment

--- Main.java
+++ Main.java
@@ -1,19 +1,11 @@
 import java.util.Scanner;
 import java.net.Socket;
-import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
-    try {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter your username for the chat");
-      String username = scanner.nextLine();
-      Socket socket = new Socket("flippinnublet.com", 1234);
-      Client client = new Client(socket, username);
-      client.messageListener();
-      client.sendMessage();
-    } catch (IOException e) {
-      e.printStackTrace();
-    } 
+    System.out.println("Give a name: ");
+    Scanner input = new Scanner(System.in);
+    String name = input.next();
+    Player player = new Player(name);
   }
 }
