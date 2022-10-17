541177cc07b79a97595cba7bb405a3276af5ad7e
TheFlyingJeep <90123966+TheFlyingJeep@users.noreply.github.com>
2022-10-17 09:07:37 -0400

    pain

--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.ArrayList;
+import java.io.IOException;
 import java.net.Socket;
 
 class Player {
@@ -7,40 +8,33 @@ class Player {
   int lives = 3;
   ArrayList<String> lettersAvail = new ArrayList<String>();
   boolean isDead = false;
-  boolean isTurn = true;
-  GUI gui = new GUI();
+  boolean isTurn = false; 
+  GUI gui;
+  String guess = "";
   Client client;
   Socket socket;
-  String guess = "";
-  Thread thread;
-  String data = "";
-  ArrayList<String> names = new ArrayList<String>();
+  Thread clientThread;
+  int playerNumber;
 
-  public Player(Socket socket1, String name) {
-    playerName = name;
-    socket = socket1;
-    client = new Client(socket);
-    thread = new Thread(client);
-    thread.start();
+  public Player(String name) {
     try {
-      Thread.sleep(10);
-    } catch (InterruptedException e) {
+      socket = new Socket("flippinnublet.com", 7268);
+    } catch (IOException e) {
       e.printStackTrace();
     }
+    playerName = name;
+    gui = new GUI();
+    client = new Client(socket);
+    clientThread = new Thread(client);
+    clientThread.start();
+    // try {
+    //   Thread.sleep(100);
+    // } catch (InterruptedException e) {
+    //   e.printStackTrace();
+    // } 
     client.sendGuess(name);
-    // System.out.println("hello!");
     theLoop();
   }
-
-  public void getGuess() {
-    // if (gui.isValid()) {
-    // guess = gui.sendCInput();
-    // System.out.println(guess);
-    // gui.updateLives(lives - 1);
-    // System.out.println(lives);
-  }
-
-  // }
 
   public void letterAdd() {
     lettersAvail.add("A");
@@ -67,27 +61,24 @@ class Player {
     lettersAvail.add("U");
   }
 
-  public int lifeRemove(boolean timeGone) {
-    if (timeGone) {
-      lives--;
+    public int lifeRemove(boolean timeGone) {
+      if (timeGone) {
+        lives--;
+      }
+      return lives;
     }
-    return lives;
-  }
 
-  public int lifeAdd() {
-    if (lettersAvail.size() == 0) {
-      lives++;
+    public int lifeAdd() {
+      if (lettersAvail.size() == 0) {
+        lives++; 
+      }
+      return lives;
     }
-    return lives;
-  }
 
-  public ArrayList<String> updateUsedLetters(String word) {
-    for (int j = 0; j < word.length(); j++) {
-      String letter = word.substring(j, j + 1);
-
+    public ArrayList<String> updateUsedLetters(String used) {
       for (int i = 0; i < lettersAvail.size(); i++) {
         String a = lettersAvail.get(i);
-        if (letter.equals(a)) {
+        if (used.equals(a)) {
           lettersAvail.remove(i);
         }
       }
@@ -95,91 +86,84 @@ class Player {
       if (lettersAvail.size() == 0) {
         letterAdd();
       }
-
+      return lettersAvail;
     }
-    /*
-     * for (String i : lettersAvail) {
-     * System.out.print(i + " ");
-     * }
-     */
-    gui.updateLetters(lettersAvail);
-    return lettersAvail;
-  }
-
-  public String sendGuess() {
-    return guess;
-  }
-  /*
-   * public ArrayList<String> sepLetters(String word) {
-   * ArrayList<String> letters = new ArrayList<String>();
-   * for (int i = 0; i < word.length(); i++) {
-   * letters.add(word);
-   * }
-   * return letters;
-   * }
-   */
 
   public ArrayList<String> sendLetters() {
     return lettersAvail;
-  }
-
-  public void sendLives() {
-    gui.updateLives(lives);
-  }
-
-  public boolean sendTurn() {
-    return isTurn;
-  }
-
-  public void sendPrompts() {
-    gui.setPrompt(data);
-  }
-
-  public void sendNames(String[] names) {
-    gui.setNames(names);
-  }
+  } 
 
   public void theLoop() {
-    // System.out.print("is dead =" + isDead);
-
-    while (!isDead) {
-      guess = gui.sendCInput();
-
-      if (!guess.equalsIgnoreCase("")) {
-        if (guess != null) {
-          // System.out.println(guess);
+    while (true) {
+      String data = client.data;
+      System.out.println((data != null) ? data : "null");
+      if (!data.equals("") && data != null) {
+        if (data.equals("turn") && !isDead) {
+          isTurn = true;
         }
-        client.sendGuess(guess);
-        gui.resetGuesses();
-        // System.out.println("Hello!");
-        data = client.data;
-        String[] dataArr;
-
-        if (data != null) {
-          dataArr = data.split(",");
-        } else {
-          break;
+        String[] dataArr = data.split(",");
+        if (dataArr != null &&  !data.equals("turn")) {
+          switch (dataArr[0].toLowerCase()) {
+            case "prompt":
+              gui.getPrompt(dataArr);
+              break;
+            case "names":
+              gui.getNames(dataArr);
+              client.data = "";
+              break;
+            case "lives":
+              gui.getLives(dataArr);
+              client.data = "";
+              break;
+            case "correct":
+              gui.setInputDisplay(dataArr[1]);
+              if (isTurn) {
+                System.out.println("YAY");
+                isTurn = false;
+              } else {
+                System.out.println("Other guy did good");
+              }
+              break;
+            case "incorrect":
+              gui.setInputDisplay(dataArr[1]);
+              if (isTurn) {
+                System.out.println("AW");
+              } else {
+                System.out.println("Other oops");
+              }
+              break;
+            case "timer":
+              if (isTurn) {
+                lives--;
+                gui.updateLives(lives);
+                if (lives <= 0) {
+                  isDead = true;
+                }
+                isTurn = false;
+              } else {
+                System.out.println("Other guy died");
+              }
+              break;
+            default:
+              break;
+          }
         }
-
-        if (dataArr[0].equals("names")) {
-          sendNames(dataArr);
-        }
-
-        if (dataArr[0].equals("lives")) {
-          sendLives();
-        }
-
-        if (dataArr[0].equals("correct")) {
-
-        }
-
+        dataArr = null;
+        data = "";
+        client.data = "";
       }
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+      if (isTurn) {
+        String guess = gui.sendCInput();
+        if (!guess.equalsIgnoreCase("")) {
+          client.sendGuess(guess);
+          gui.resetGuesses();
+        }
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
       }
     }
   }
-
 }
