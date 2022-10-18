348e3c92659248e6dc735a09ea12283ad743e130
grass <81576338+Grassias10@users.noreply.github.com>
2022-10-18 11:31:07 -0400

    10/18 main push

--- Main.java
+++ Main.java
@@ -3,9 +3,6 @@ import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Give a name: ");
-    Scanner input = new Scanner(System.in);
-    String name = input.next();
-    Player player = new Player(name);
+    Player player = new Player();
   }
 }
